1207dbf8ca1c4e451ac8342e170823160df6c828
Erdem Üveyik <73111211+ErdemBey1@users.noreply.github.com>
2021-02-23 14:28:33 +0300

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Space UserBot - The Miri - Dadashzade
+# Siri UserBot - ErdemBey - Midy
 
 """ UserBot başlangıç noktası """
 import importlib

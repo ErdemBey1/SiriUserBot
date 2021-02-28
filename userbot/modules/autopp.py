1f0638af2e363436350a02463291b4d601e948ae
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-02-28 16:24:00 +0300

    Update autopp.py

--- userbot/modules/autopp.py
+++ userbot/modules/autopp.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# SpaceUserBot - The Miri - Dadashzade
+# SSiriUserBot - ErdemBey - Midy
 
 # @NaytSeyd tarafından portlanmıştır.
 # @frknkrc44 tarafından düzenlenmiştir.

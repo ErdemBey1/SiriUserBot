e185303020408d103c7b71786db506021697ea7a
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-02-28 17:30:24 +0300

    Update shazam.py

--- userbot/modules/shazam.py
+++ userbot/modules/shazam.py
@@ -4,10 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
-# Telegram @Fusuf
-
-# Coded by @Fusuf
+# SiriUserBot - ErdemBey - Midy
 
 from pydub import AudioSegment
 from json import dumps

beaf4ce6529d8139674062f8ce04e18052b56bb5
must4f <75228263+must4f@users.noreply.github.com>
2021-03-06 16:07:53 +0300

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -14,7 +14,7 @@ from asyncio.subprocess import PIPE as asyncPIPE
 from platform import uname
 from shutil import which
 from os import remove
-from userbot import CMD_HELP, SIRI_VERSION, DEFAULT_NAME
+from userbot import CMD_HELP, SIRI_VERSION, DEFAULT_NAME, WHITELIST
 from userbot.events import register
 from userbot.main import PLUGIN_MESAJLAR
 from telethon import version

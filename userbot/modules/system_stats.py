77e67b6dfb20f9f72ac5858bfbae1a780b9ef0a6
must4f <75228263+must4f@users.noreply.github.com>
2021-03-15 23:48:11 +0300

    : Siri Update

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -15,6 +15,9 @@ from platform import uname
 from shutil import which
 from os import remove
 from userbot import CMD_HELP, SIRI_VERSION, DEFAULT_NAME, WHITELIST
+from userbot import uid as MYID
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
 from userbot.events import register
 from userbot.main import PLUGIN_MESAJLAR
 from telethon import version
@@ -30,6 +33,8 @@ LANG = get_value("system_stats")
 
 # ████████████████████████████████ #
 # ============================================
+
+
 
 @register(outgoing=True, pattern="^.sysd$")
 async def sysdetails(sysd):
@@ -134,7 +139,7 @@ async def amialive(e):
     if DEFAULT_NAME:
         sahipp = f"{DEFAULT_NAME}"
     else:
-        sahipp = "**Sir**"
+        sahipp = "Sir"
     me = await e.client.get_me()
     if type(PLUGIN_MESAJLAR['alive']) == str:
         await e.edit(PLUGIN_MESAJLAR['alive'].format(
@@ -169,12 +174,72 @@ async def amialive(e):
         else:
             await e.respond(PLUGIN_MESAJLAR['alive'])
 
-@register(incoming=True, from_users=WHITELIST, pattern="^.walive$")
+@register(incoming=True, from_users=WHITELIST, pattern="^.wlive$")
 async def wwwwailve(event):
+    f_q, error_i_a = await get_full_user(event)
+    if f_q is None:
+        return False
     if event.fwd_from:
         return
-    elif event.is_reply:
+    elif f_q.user.user_id == MYID:
         await event.reply("`Yöneticim çalışıp çalışmadığımı kontrol ediyor! Endişelenmeyin..`")
+
+
+async def get_full_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.forward.from_id or previous_message.forward.channel_id
+                )
+            )
+            return replied_user, None
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.from_id
+                )
+            )
+            return replied_user, None
+    else:
+        input_str = None
+        try:
+            input_str = event.pattern_match.group(1)
+        except IndexError as e:
+            return None, e
+        if event.message.entities is not None:
+            mention_entity = event.message.entities
+            probable_user_mention_entity = mention_entity[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            else:
+                try:
+                    user_object = await event.client.get_entity(input_str)
+                    user_id = user_object.id
+                    replied_user = await event.client(GetFullUserRequest(user_id))
+                    return replied_user, None
+                except Exception as e:
+                    return None, e
+        elif event.is_private:
+            try:
+                user_id = event.chat_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+        else:
+            try:
+                user_object = await event.client.get_entity(int(input_str))
+                user_id = user_object.id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+
+
 
 CmdHelp('system_stats').add_command(
     'sysd', None, 'Neofetch modülünü kullanarak sistem bilgisi gösterir.'

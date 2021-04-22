e0d7b26f1f0f17cf00272afc4b6d70b2905d1d75
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-04-22 18:50:18 +0300

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -291,7 +291,7 @@ async def upload_file(http, file_path, file_name, mime_type, event, parent_id):
     media_body = MediaFileUpload(file_path, mimetype=mime_type, resumable=True)
     body = {
         "title": file_name,
-        "description": "Asena UserBot kullanılarak yüklendi.",
+        "description": "Siri UserBot kullanılarak yüklendi.",
         "mimeType": mime_type,
     }
     if parent_id:

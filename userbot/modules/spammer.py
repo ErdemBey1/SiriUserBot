b505c308f866fa1f6bb708add995b7558d8c49ce
berce <75228263+must4f@users.noreply.github.com>
2021-05-02 20:56:43 +0300

    spam fix

--- userbot/modules/spammer.py
+++ userbot/modules/spammer.py
@@ -30,13 +30,11 @@ async def tmeme(e):
                 "TSpam başarıyla gerçekleştirildi"
                 )
 
-@register(outgoing=True, pattern="^.spam$")
+@register(outgoing=True, pattern="^.spam(?: |$)(.*)")
 async def spammer(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        message = e.text
-        messageSplit = message.split(" ", 2)
-        counter = int(messageSplit[1])
-        spam_message = str(messageSplit[2])
+        counter = event.pattern_match.group(1)
+        spam_message = event.pattern_match.group(2)
         await asyncio.wait([e.respond(spam_message) for i in range(counter)])
         await e.delete()
         if BOTLOG:

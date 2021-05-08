fd1076cb8fe2e9875ac6e6ffecc60214217f5a88
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-05-08 17:32:40 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -216,7 +216,7 @@ HEROKU_APPNAME = os.environ.get("HEROKU_APPNAME", None)
 HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
 
 # Güncelleyici için repo linki.
-UPSTREAM_REPO_URL = "https://github.com/SiriUserBot/SiriUserBot.git"
+UPSTREAM_REPO_URL = "https://github.com/ErdemBey1/SiriUserBot.git"
 
 # Bot versiyon kontrolü
 forceVer = []
